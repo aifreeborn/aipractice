71a932beb56c5d20e6f500bf93cd9fe59d5bf65b
takeno2020 <takeno2020@163.com>
2020-06-27 15:35:10 +0800

    CMake练习程序：基于官方的Tutorial示例程序.

---
+++ aicmake/tutorial/README.md
@@ -0,0 +1,2 @@
+根据CMake官方的demo练习该编译系统的使用，CMake Tutorial地址：
+https://cmake.org/cmake/help/v3.18/guide/tutorial/index.html
---
+++ aicmake/tutorial/test/hello.c
@@ -0,0 +1,26 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2020年06月23日 10时52分24秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  takeno (takeno2020), takeno2020@163.com
+ *   Organization:  
+ *
+ * =============================================================================
+ */
+#include <stdio.h>
+
+/************************************ main ************************************/
+int main(int argc, char *argv[])
+{
+    printf("hello\n");
+
+    return 0;
+}
