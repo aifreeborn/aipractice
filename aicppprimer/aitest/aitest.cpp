c1976f20ad33ebb396257716b2b5cac8b0f0e5f7
takeno <takeno2020@163.com>
2020-10-11 07:17:29 +0800

    添加C++知识点等练习、测试的程序模板文件.

---
+++ aicppprimer/aitest/aitest.cpp
@@ -0,0 +1,27 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  aitest.cpp
+ *
+ *    Description: 用于练习小程序和C++学习的知识点 
+ *
+ *        Version:  1.0
+ *        Created:  2020年10月11日 07时15分00秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  takeno (takeno2020), takeno2020@163.com
+ *   Organization:  
+ *
+ * =============================================================================
+ */
+#include <iostream>
+#include <cstdlib>
+
+/************************************ main ************************************/
+int main(int argc, char *argv[])
+{
+
+
+    return EXIT_SUCCESS;
+}
